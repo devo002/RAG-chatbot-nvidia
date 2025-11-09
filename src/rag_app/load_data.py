11804da408a4e2e13012897faae3580ac06f6e05
David_Onaiyekan <smile2devo.dm@gmail.com>
2025-11-09 19:03:53 +0100

    add requirements

--- src/rag_app/load_data.py
+++ src/rag_app/load_data.py
@@ -1,4 +1,5 @@
 from langchain.document_loaders import PyPDFLoader
+#from langchain_community.document_loaders import PyPDFLoader
 from langchain.document_loaders.html import UnstructuredHTMLLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
